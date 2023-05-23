55aa682103fa8e800c9f4fa64e1f95729a9b77a9
DaviPacini <126252677+DaviPacini@users.noreply.github.com>
2023-05-23 17:33:58 -0300

    Add files via upload

--- aula1/ex4.py
+++ aula1/ex4.py
@@ -2,6 +2,12 @@ altura = float(input("Informe a altura: \n"))
 peso = float(input("Informe o peso: \n"))
 imc = peso/(altura*altura)
 if imc < 18.5:
-print("Você está abaixo do peso ideal, com o imc de: %.2f"%(imc))
+    print("Você está abaixo do peso ideal, com o imc de: %.2f"%(imc))
+elif imc >= 18.5 and imc < 25.0:
+    print("Você está no peso ideal, com ic de: %.2f"%(imc))
+elif imc >= 25 and imc < 30 :
+    print("Você está com sobrepeso, com imc de: %.2f"%(imc))
+elif imc >= 30:
+    print("Você está com obesidade, com imc de: %.2f"%(imc))
 
 
