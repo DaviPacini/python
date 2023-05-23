499594112228c4501dc4554b1b1f858cee91b12e
DaviPacini <126252677+DaviPacini@users.noreply.github.com>
2023-05-23 18:27:30 -0300

    Add files via upload

---
+++ aula1/exifnotas.py
@@ -0,0 +1,17 @@
+n1 = float(input("Informe a primeira nota: \n"))
+if 0 <= n1 <= 10:
+    n2 = float(input("Informe a segunda nota: \n"))
+    if 0 <= n2 <= 10:
+        n3 = float(input("Informe a terceira nota: \n"))
+        if 0 <= n3 <= 10:
+            media = (n1 + n2 + n3)/3
+            if media >= 7:
+                print("O aluno obteve média %.f, e foi APROVADO"%(media))
+            else:
+                print("O aluno obteve Média %.f, e foi REPROVADO"%(media))
+        else:
+            print("Valor inválido")
+    else:
+        print("Valor inválido")
+else:
+    print("Valor inválido")
