952f69aafad2b88897389ccb01e15b44d5a9acd7
DaviPacini <davi.pacini@sempreceub.com>
2023-06-06 20:10:52 -0300

    fibonacci

--- exercicioscasa/ex_fibonacci_for.py
+++ exercicioscasa/ex_fibonacci_for.py
@@ -1,7 +1,7 @@
 n = int(input("Quantos números da tabela você deseja ver?\n"))
 i = 2
 z = 1
-fibonacci = [1, 1]
+fibonacci = [0, 1]
 if n == 1 or n == 2:
     print(fibonacci[n-1])
 
@@ -10,5 +10,5 @@ else:
     while i < n:
         y = fibonacci[i-1] + fibonacci[i - 2]
         fibonacci.append(y)
-        i +=
+        i += 1
     print(fibonacci)
